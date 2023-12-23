595c54adb200b694381b98e8c058468f1a5dbf4b
yoyodes1000 <yoyodes1000@gmail.com>
2023-12-23 23:35:05 +0100

    quete 10 validdée

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,18 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $program,
         ]);
     }
 
     #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
-    public function show(): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'parametre' => 4,
+            'program' => $program,
         ]);
     }
 }
